e9a7e54de72ae0504bc865f4bf119a75d96a337c
kmthein <kmthein96.mm@gmail.com>
2024-10-12 12:52:32 +0630

    annouce process

--- src/app/page/admin/annoucement-list/annoucement-list.component.ts
+++ src/app/page/admin/annoucement-list/annoucement-list.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-annoucement-list',
   templateUrl: './annoucement-list.component.html',
-  styleUrl: './annoucement-list.component.css'
+  styleUrl: './annoucement-list.component.css',
 })
 export class AnnoucementListComponent {
+  isLoading: boolean = false;
+  isModalOpen: boolean = false;
+  selectedFile: File | null = null;
 
+  onFileSelected(event: any): void {
+    this.selectedFile = event.target.files[0];
+  }
+
+  toggleModal() {
+    this.isModalOpen = !this.isModalOpen;
+    initFlowbite();
+  }
 }
