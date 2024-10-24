aac0246ef2c7b30fd28381583bfa3879dd6be90f
kmthein <kmthein96.mm@gmail.com>
2024-10-25 03:27:53 +0630

    lunch add done

--- src/app/services/lunch.service.ts
+++ src/app/services/lunch.service.ts
@@ -4,12 +4,16 @@ import { Observable } from 'rxjs';
 import { Lunch } from '../models/lunch';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LunchService {
   private apiUrl = 'http://localhost:8080/api/lunches'; // Adjust this to your API endpoint
 
   constructor(private http: HttpClient) {}
+
+  addWeeklyLunch(formData: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/weekly`, formData);
+  }
 
   // Get all lunches
   getAllLunches(): Observable<Lunch[]> {
