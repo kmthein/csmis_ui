858c884bf9bb8cd8c8cd63d79230ffbb6e562b8f
kmthein <kmthein96.mm@gmail.com>
2024-10-24 07:23:39 +0630

    announce update done

--- src/app/page/admin/annoucement-list/annoucement-list.component.ts
+++ src/app/page/admin/annoucement-list/annoucement-list.component.ts
@@ -24,10 +24,11 @@ export class AnnoucementListComponent implements OnInit, OnDestroy {
   editorContent: string = '';
   uploadProgress: number | undefined;
   downloadURL: string | undefined;
-  selectedAnnouncement: any;  
+  selectedAnnouncement: any;
   editMode: boolean = false;
   hoveredIndex: any = null;
   deleteFileIds: any = [];
+  previewFiles: any = [];
 
   constructor(
     private authService: AuthService,
@@ -38,6 +39,7 @@ export class AnnoucementListComponent implements OnInit, OnDestroy {
 
   onSelectAnnouncement(announcement: any) {
     this.selectedAnnouncement = announcement;
+    this.previewFiles = announcement?.files;
     this.title = announcement.title;
     this.editorContent = announcement.content;
     console.log(this.selectedAnnouncement);
@@ -46,19 +48,14 @@ export class AnnoucementListComponent implements OnInit, OnDestroy {
     this.toggleModal(true);
   }
 
-  deleteFile(id: number) {
-    this.deleteFileIds.push(id);
-    console.log(this.deleteFileIds);
-  }
-
   toggleModal(editMode: boolean) {
     this.isModalOpen = !this.isModalOpen;
-    if(!editMode) {
+    if (!editMode) {
       this.title = '';
       this.editorContent = '';
       this.editMode = false;
     }
-    if(this.isModalOpen) {
+    if (this.isModalOpen) {
       this.renderer.addClass(document.body, 'overflow-hidden');
     } else {
       this.renderer.removeClass(document.body, 'overflow-hidden');
@@ -73,7 +70,7 @@ export class AnnoucementListComponent implements OnInit, OnDestroy {
     this.announceService.getAllAnnouncements().subscribe((data) => {
       console.log(data);
       this.announcements = data;
-    })
+    });
   }
 
   ngOnInit(): void {
@@ -98,80 +95,124 @@ export class AnnoucementListComponent implements OnInit, OnDestroy {
     ['align_left', 'align_center', 'align_right', 'align_justify'],
   ];
 
-selectedFiles: File[] = []; 
-renamedFiles: File[] = [];  
+  selectedFiles: File[] = [];
+  renamedFiles: File[] = [];
 
-onFileSelected(event: any): void {
-  this.selectedFiles = Array.from(event.target.files); 
-}
+  deleteFile(id: number) {
+    this.deleteFileIds.push(id);
+    console.log(this.deleteFileIds);
+    const newPreview = this.previewFiles.filter((file: any) => file.id != id);
+    this.previewFiles = newPreview;
+    console.log(this.previewFiles);
+  }
 
-uploadFiles() {
-  const uploadPromises = this.selectedFiles.map((file) => this.uploadFile(file)); 
-  Promise.all(uploadPromises)
-    .then((renamedFiles) => {
-      const id = this.user?.id;
-      this.announceService
-        .addNewAnnouncement(
-          this.title,
-          this.editorContent,
-          renamedFiles,  
-          id!
-        )
-        .subscribe({
-          next: (response) => {
-            if (response.message) {
-              this.getAllAnnouncement();
-              this.isLoading = false;
-              this.toggleModal(false);
-              this.title = '';
-              this.editorContent = '';
-            }
-          },
-          error: (error) => {
-            console.error(error);
-          },
-        });
-    })
-    .catch((error) => {
-      console.error('Error uploading files:', error);
-    });
-}
+  onFileSelected(event: any): void {
+    this.selectedFiles = Array.from(event.target.files);
+  }
 
-uploadFile(file: File): Promise<File> {
-  const filePath = `${file.name}`;
-  const fileRef = this.storage.ref(filePath);
-  const task = this.storage.upload(filePath, file);
-
-  return new Promise((resolve, reject) => {
-    task.percentageChanges().subscribe((progress) => {
-      this.uploadProgress = progress;
-    });
-
-    task.snapshotChanges().pipe(
-      finalize(() => {
-        fileRef.getDownloadURL().subscribe(
-          (url) => {
-            console.log(url);
-            const renamedFile = this.renameFile(file, url);
-            this.renamedFiles.push(renamedFile);
-            resolve(renamedFile); 
-          },
-          (error) => reject(error)  
-        );
+  uploadFiles() {
+    const uploadPromises = this.selectedFiles.map((file) =>
+      this.uploadFile(file)
+    );
+    Promise.all(uploadPromises)
+      .then((renamedFiles) => {
+        const id = this.user?.id;
+        if (this.editMode) {
+          this.announceService
+            .updateAnnouncement(
+              this.selectedAnnouncement?.id,
+              this.title,
+              this.editorContent,
+              renamedFiles,
+              id!,
+              this.deleteFileIds
+            )
+            .subscribe({
+              next: (response) => {
+                if (response.message) {
+                  this.getAllAnnouncement();
+                  this.isLoading = false;
+                  this.toggleModal(false);
+                  this.title = '';
+                  this.editorContent = '';
+                  this.selectedAnnouncement = null;
+                  this.deleteFileIds = [];
+                  this.editMode = false;
+                  this.previewFiles = [];
+                  this.selectedFiles = [];
+                }
+              },
+              error: (error) => {
+                console.error(error);
+              },
+            });
+        } else {
+          this.announceService
+            .addNewAnnouncement(
+              this.title,
+              this.editorContent,
+              renamedFiles,
+              id!
+            )
+            .subscribe({
+              next: (response) => {
+                if (response.message) {
+                  this.getAllAnnouncement();
+                  this.isLoading = false;
+                  this.toggleModal(false);
+                  this.title = '';
+                  this.editorContent = '';
+                }
+              },
+              error: (error) => {
+                console.error(error);
+              },
+            });
+        }
       })
-    ).subscribe();
-  });
-}
+      .catch((error) => {
+        console.error('Error uploading files:', error);
+      });
+  }
 
-renameFile(originalFile: File, newFileName: string): File {
-  const renamedFile = new File([originalFile], newFileName, {
-    type: originalFile.type,
-    lastModified: originalFile.lastModified,
-  });
-  return renamedFile;
-}
+  uploadFile(file: File): Promise<File> {
+    const filePath = `${file.name}`;
+    const fileRef = this.storage.ref(filePath);
+    const task = this.storage.upload(filePath, file);
 
-onSubmit() {
+    return new Promise((resolve, reject) => {
+      task.percentageChanges().subscribe((progress) => {
+        this.uploadProgress = progress;
+      });
+
+      task
+        .snapshotChanges()
+        .pipe(
+          finalize(() => {
+            fileRef.getDownloadURL().subscribe(
+              (url) => {
+                console.log(url);
+                const renamedFile = this.renameFile(file, url);
+                this.renamedFiles.push(renamedFile);
+                resolve(renamedFile);
+              },
+              (error) => reject(error)
+            );
+          })
+        )
+        .subscribe();
+    });
+  }
+
+  renameFile(originalFile: File, newFileName: string): File {
+    const renamedFile = new File([originalFile], newFileName, {
+      type: originalFile.type,
+      lastModified: originalFile.lastModified,
+    });
+    return renamedFile;
+  }
+
+  onSubmit() {
     this.uploadFiles();
-}
+  }
 }
--- src/app/services/annoucement/annoucement.service.ts
+++ src/app/services/annoucement/annoucement.service.ts
@@ -26,15 +26,52 @@ export class AnnoucementService {
     const formData = new FormData();
     formData.append('title', title);
     formData.append('content', content);
-    for (let i = 0; i < files.length; i++) {
-      formData.append('files', files[i]);
+    if (files.length > 0) {
+      for (let i = 0; i < files.length; i++) {
+        formData.append('files', files[i]);
+      }
     }
     formData.append('user', `${admin}`);
     formData.append('date', formattedDate);
     return this.http.post<any>(`${this.url}/announcements`, formData).pipe(
       tap((response) => {
         console.log(response);
-        if(response.message) {
+        if (response.message) {
+          this.toast.success(response.message);
+        } else if (response.error) {
+          this.toast.error(response.error);
+        }
+      })
+    );
+  }
+
+  updateAnnouncement(
+    id: number,
+    title: string,
+    content: string,
+    files: any,
+    adminId: number,
+    deleteFileIds: any
+  ) {
+    const localDate = new Date();
+    const formattedDate = localDate.toISOString().split('T')[0];
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('content', content);
+    if (files.length > 0) {
+      for (let i = 0; i < files.length; i++) {
+        formData.append('newFiles', files[i]);
+      }
+    }
+    formData.append('adminId', `${adminId}`);
+    formData.append('date', formattedDate);
+    if (deleteFileIds.length > 0) {
+      formData.append('deleteFileIds', deleteFileIds);
+    }
+    return this.http.put<any>(`${this.url}/announcements/${id}`, formData).pipe(
+      tap((response) => {
+        console.log(response);
+        if (response.message) {
           this.toast.success(response.message);
         } else if (response.error) {
           this.toast.error(response.error);
